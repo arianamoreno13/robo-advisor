70fbb7a99e1174489a4bd8098f32d43b37323d7e
ariana <ariana.moreno13@yahoo.com>
2023-02-09 22:22:43 -0700

    folder structure updated

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # robo-advisor
-using the AWS Management Console I made a robo advisor to get investment portfolio recommendations for retirement.
+An AWS-built investment advisor bot is an artificial intelligence tool that provides personalized investment recommendations to users based on various data inputs, including their age, and risk tolerance. This bot leverages the power of machine learning algorithms and natural language processing techniques to analyze vast amounts of financial data and make informed investment decisions. The bot can interact with users through a user-friendly interface, such as a chatbot, and can be integrated into existing financial applications. 
--- Starter_Code/Lambda/lambda_function.py
+++ Starter_Code/Lambda/lambda_function.py
@@ -80,36 +80,6 @@ def close(session_attributes, fulfillment_state, message):
     return response
 
 
-"""
-Step 3: Enhance the Robo Advisor with an Amazon Lambda Function
-
-In this section, you will create an Amazon Lambda function that will validate the data provided by the user on the Robo Advisor.
-
-1. Start by creating a new Lambda function from scratch and name it `recommendPortfolio`. Select Python 3.7 as runtime.
-
-2. In the Lambda function code editor, continue by deleting the AWS generated default lines of code, then paste in the starter code provided in `lambda_function.py`.
-
-3. Complete the `recommend_portfolio()` function by adding these validation rules:
-
-    * The `age` should be greater than zero and less than 65.
-    * The `investment_amount` should be equal to or greater than 5000.
-
-4. Once the intent is fulfilled, the bot should respond with an investment recommendation based on the selected risk level as follows:
-
-    * **none:** "100% bonds (AGG), 0% equities (SPY)"
-    * **low:** "60% bonds (AGG), 40% equities (SPY)"
-    * **medium:** "40% bonds (AGG), 60% equities (SPY)"
-    * **high:** "20% bonds (AGG), 80% equities (SPY)"
-
-> **Hint:** Be creative while coding your solution, you can have all the code on the `recommend_portfolio()` function, or you can split the functionality across different functions, put your Python coding skills in action!
-
-5. Once you finish coding your Lambda function, test it using the sample test events provided for this Challenge.
-
-6. After successfully testing your code, open the Amazon Lex Console and navigate to the `recommendPortfolio` bot configuration, integrate your new Lambda function by selecting it in the “Lambda initialization and validation” and “Fulfillment” sections.
-
-7. Build your bot, and test it with valid and invalid data for the slots.
-
-"""
 
 
 ### Intents Handlers ###
@@ -176,13 +146,13 @@ def get_recommendation(risk_level):
     elif risk_level == 'High':
         return '20% bonds (AGG), 80% equities (SPY).'
     else:
-        return '100% bonds (AGG), 0% equities (SPY) - (default portfolio recommended, Invalid risk-level).'
+        return 'Please indicate risk level: None, Low, Medium, or High'
 
 ### validates age and investment amount ###
 def validate_inputs(age, investment_amount):
     """ This line of code makes sure age is valid so you don't try and change NaN to float """
     if age is not None:
-        age = float(age)
+        age = int(age)
 
         """ This line of code tells user they need to submit a valid age (age constraint) """
         if age <= 0 or age >= 65:
@@ -194,18 +164,17 @@ def validate_inputs(age, investment_amount):
 
     """ Checks to make sure investment amount is valid so you don't try and change NaN to float """
     if investment_amount is not None:
-        investment_amount = float(investment_amount)
+        investment_amount = int(investment_amount)
 
         """ Checks against constraint (investment constraint) """
         if investment_amount < 5000:
             return build_validation_result(
                 False,
                 'investmentAmount',
-                'The investment amount entered is invalid. Please enter an amount greater than or equal to $5000.'
+                'The investment amount entered is invalid, please provide an amount greater than or equal to $5000.'
             )
 
     return build_validation_result(True, None, None)
-
 
 ### Intents Dispatcher ###
 def dispatch(intent_request):
@@ -229,4 +198,4 @@ def lambda_handler(event, context):
     The JSON body of the request is provided in the event slot.
     """
 
-    return dispatch(event)
+    return(event)
